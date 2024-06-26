ee06b2ade4337555cd9184b0fc8c8a1b5d0ab370
jainisheridan <133915511+jainisheridan@users.noreply.github.com>
2024-06-26 17:56:35 -0400

    Add files via upload

---
+++ Final-JainiTopiwala/Final-JainiTopiwala/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Final_JainiTopiwala
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btngetEmp_Click(object sender, RoutedEventArgs e)
+        {
+            var employees = db.Employees.Select(emp => new
+            {
+                emp.EmployeeID, emp.FirstName, emp.LastName, emp.Title, BirthDate = emp.BirthDate
+            }).ToList();
+
+            grdEmployees.ItemsSource = employees;
+        }
+
+        private void btnEmpbyfname_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName = txtFirstname.Text; 
+            var employees = db.Employees.Where(emp => emp.FirstName.Contains(firstName)).Select(emp => new
+            {
+                emp.EmployeeID, emp.FirstName, emp.LastName, emp.Title, BirthDate = emp.BirthDate
+            }).ToList();
+
+            grdEmployees.ItemsSource = employees;
+        }
+
+        private void btnEmpbyage_Click(object sender, RoutedEventArgs e)
+        {
+            var employees = db.Employees.Select(emp => new
+            {
+                emp.EmployeeID, emp.FirstName, emp.LastName, emp.Title,
+                BirthDate = emp.BirthDate, Age = DateTime.Now.Year - emp.BirthDate.Value.Year
+            }).OrderByDescending(emp => emp.Age).ToList();
+
+            grdEmployees.ItemsSource = employees;
+        }
+
+        private void btngetOrders_Click(object sender, RoutedEventArgs e)
+        {
+            var employees = db.Employees.Select(emp => new
+            {
+                emp.EmployeeID, emp.FirstName, emp.LastName, emp.Title,
+                BirthDate = emp.BirthDate, Age = DateTime.Now.Year - emp.BirthDate.Value.Year
+            }).OrderByDescending(emp => emp.Age).ToList();
+
+            grdEmployees.ItemsSource = employees;
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
